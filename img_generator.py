74dad5642e0893758171b256b4d9ca5fff1c1474
Gonzaa21 <gonzidibujante@gmail.com>
2025-06-03 16:05:43 -0300

    modified: ajust text to top

--- img_generator.py
+++ img_generator.py
@@ -13,7 +13,7 @@ def load_last_snapshot(snapshot_dir=SNAPSHOT_DIR):
     filepath = os.path.join(snapshot_dir, filename)
     return pd.read_csv(filepath), filename
 
-# save png in memory
+# save png in memory temporarily
 buf = io.BytesIO()
 
 # generate graphs for each component
@@ -26,16 +26,16 @@ def generate_graphs(df: pd.DataFrame):
     if "Operating System" in df.columns:
         try:
             os_info = json.loads(df["Operating System"].iloc[0].replace("'", "\""))
-            texts.append(f"Sistema Operativo: {os_info}")
+            texts.append(f"Operative System: {os_info}")
         except Exception:
-            texts.append(f"Sistema Operativo: {df['Operating System'].iloc[0]}")
+            texts.append(f"Operative System: {df['Operating System'].iloc[0]}")
 
     if "OS Version" in df.columns:
         try:
             version = float(df["OS Version"].iloc[0])
-            texts.append(f"Versión: {version}")
+            texts.append(f"Version: {version}")
         except:
-            texts.append(f"Versión: {df['OS Version'].iloc[0]}")
+            texts.append(f"Version: {df['OS Version'].iloc[0]}")
 
     # RAM Memory
     if all(col in df.columns for col in ["Used RAM", "Free RAM", "Available RAM"]):
@@ -45,9 +45,9 @@ def generate_graphs(df: pd.DataFrame):
             available = float(df["Available RAM"].iloc[0])
             ax_ram = fig.add_subplot(gs[0, 0])
             ax_ram.pie([used, free, available],
-                       labels=["Usada", "Libre", "Disponible"],
+                       labels=["Used", "Free", "Available"],
                        autopct="%1.1f%%", startangle=90)
-            ax_ram.set_title("Memoria RAM")
+            ax_ram.set_title("RAM Memory")
         except ValueError:
             pass
 
@@ -58,9 +58,9 @@ def generate_graphs(df: pd.DataFrame):
             free_swap = float(df["Free SWAP"].iloc[0])
             ax_swap = fig.add_subplot(gs[0, 1])
             ax_swap.pie([used_swap, free_swap],
-                        labels=["Usada", "Libre"],
+                        labels=["Used", "Free"],
                         autopct="%1.1f%%", startangle=90)
-            ax_swap.set_title("Memoria SWAP")
+            ax_swap.set_title("SWAP Memory")
         except ValueError:
             pass
 
@@ -69,27 +69,27 @@ def generate_graphs(df: pd.DataFrame):
         try:
             cpu_usage = float(df["Global CPU Usage"].iloc[0])
             ax_cpu = fig.add_subplot(gs[1, 0])
-            ax_cpu.bar(["Uso CPU"], [cpu_usage], color="orange")
+            ax_cpu.bar(["Use CPU"], [cpu_usage], color="orange")
             ax_cpu.set_ylim(0, 100)
-            ax_cpu.set_title("Uso Global de CPU (%)")
+            ax_cpu.set_title("Global use CPU (%)")
         except ValueError:
             pass
         
     if "Number of CPUs" in df.columns:
         try:
             cpus = df["Number of CPUs"].iloc[0]
-            texts.append(f"Núcleos de CPU: {cpus}")
+            texts.append(f"CPU Cores: {cpus}")
         except:
-            texts.append("Núcleos de CPU: Desconocido")
+            texts.append("CPU Cores: Unknown")
 
 
     # Processes
     if "Number of Processes" in df.columns:
         try:
             process = df["Number of Processes"].iloc[0]
-            texts.append(f"Procesos en ejecución: {process}")
+            texts.append(f"Processes: {process}")
         except:
-            texts.append("Procesos en ejecución: Desconocido")
+            texts.append("Processes in execution: Unknown")
             
     # Disk
     if all(col in df.columns for col in ["Disk Total Space", "Disk Available Space"]):
@@ -97,13 +97,13 @@ def generate_graphs(df: pd.DataFrame):
             total_disk = float(df["Disk Total Space"].iloc[0])
             available_disk = float(df["Disk Available Space"].iloc[0])
             ax_disk = fig.add_subplot(gs[1, 1])
-            ax_disk.bar(["Total", "Disponible"], [total_disk, available_disk], color=["blue", "green"])
-            ax_disk.set_title("Uso de Disco")
+            ax_disk.bar(["Total", "Available"], [total_disk, available_disk], color=["blue", "green"])
+            ax_disk.set_title("Disk Space")
         except ValueError as e:
             print(f'{e}')
     
     
-    # Network Bar chart with labels
+    # Network
     if all(col in df.columns for col in ["Net Name", "Net Down", "Net Up"]):
         try:
             name = df["Net Name"].iloc[0]
@@ -112,15 +112,16 @@ def generate_graphs(df: pd.DataFrame):
 
             ax_network = fig.add_subplot(gs[2, 0])
 
-            # Solo una barra (para un adaptador), con dos secciones apiladas
+            # bars
             ax_network.bar(name, down_network, label="Download", color="red")
             ax_network.bar(name, up_network, bottom=down_network, label="Upload", color="green")
-
-            ax_network.set_title("Uso de Red")
+            
+            # labels
+            ax_network.set_title("Network Usage")
             ax_network.set_ylabel("Bytes")
             ax_network.legend(loc="upper right")
 
-            # Etiquetas dentro de las barras (opcional)
+            # labels on bars
             ax_network.text(x=name, y=down_network/2, s=f"{int(down_network)}", ha='center', va='center', color='white')
             ax_network.text(x=name, y=down_network + up_network/2, s=f"{int(up_network)}", ha='center', va='center', color='white')
 
@@ -130,12 +131,11 @@ def generate_graphs(df: pd.DataFrame):
     # texts
     if texts:
         full_text = "\n".join(texts)
-        fig.text(0.01, 0.01, full_text, fontsize=10, va="bottom", ha="left")
-
-    fig.suptitle("Dashboard del Sistema", fontsize=16)
+        fig.text(0.01, 1, full_text, fontsize=10, va="top", ha="left")
+        
+    # Ajust layout and save to buffer
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
     fig.savefig(buf, format="png", bbox_inches="tight")
     plt.close(fig)
     
     buf.seek(0)
-# send to main
